25a9f1ccb5c10ca37ab00dc51330359061785f97
Pypkin <apalkov040490@gmail.com>
2018-09-18 14:02:09 +0300

    wrote the layout and logic of displaying information about the product

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import {Router, Route, browserHistory} from 'react-router'
 import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
+import Phone from 'containers/phone'
 
 const store = createStore (reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -25,6 +26,7 @@ ReactDOM.render(
             <Route component={Layout}>
                 <Route path='/' component={Phones}/>
             </Route>
+            <Route path='phones/:id' component={Phone}/>
         </Router>
     </Provider>,
     document.getElementById('root')
